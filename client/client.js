2bac9e92dc20cec57d7d43a9613ed377ad72f357
markjasonanderson <manderson@dius.com.au>
2013-11-19 16:53:30 +1100

    reworked to use intellient.li.js on the meteor client

--- client/client.js
+++ client/client.js
@@ -1,62 +1,67 @@
-Feeds = new Meteor.Collection("feeds");
-Samples = new Meteor.Collection("samples");
-
 var clientId = uuid.v4();
 var observer = null;
 var previousValue = 0;
 
 Meteor.autorun(function () {
-    Meteor.subscribe("feeds");
+  Meteor.subscribe("feeds");
 });
 
+var feeds = Session.set("feeds", []); 
+
+Template.ili_example.feeds = function(){
+  return Session.get("feeds");
+}  
+
 Meteor.startup(function() {
-    Meteor.call('keepalive', clientId);
+  ili_Api.initialise(clientId);
+  Meteor.call('keepalive', clientId);
+  ili_FeedCache.query("*", function(result) { 
+    Session.set("feeds", result.values());
+  });
 });
 
 Meteor.setInterval(function () {
-    console.log("keepalive " + clientId);
-    Meteor.call('keepalive', clientId);
+  console.log("keepalive " + clientId);
+  Meteor.call('keepalive', clientId);
 }, 20000);
 
-Template.ili_example.feeds = function () {
-    return Feeds.find({}).fetch();
-};
-
 function updateSample(value) {
-    if (value) {
-        console.log("got new value: '" + value + "', previousValue: '" + previousValue + "'");
-        d3.select(".samplev")
-            .text(previousValue)
-            .transition()
-            .duration(1000)
-            .ease('linear')
-            .tween("text", function() {
-                var i = d3.interpolate(this.textContent, value);
-                return function(t) {
-                    this.textContent = Number(i(t)).toFixed(4);
-                };
-            });
-        previousValue = value;
-    }
+  if (value) {
+    console.log("got new value: '" + value + "', previousValue: '" + previousValue + "'");
+    d3.select(".samplev")
+      .text(previousValue)
+      .transition()
+      .duration(1000)
+      .ease('linear')
+      .tween("text", function() {
+        var i = d3.interpolate(this.textContent, value);
+        return function(t) {
+          this.textContent = Number(i(t)).toFixed(4);
+        };
+      });
+    previousValue = value;
+  }
 }
 var sampleSub = null;
+var currentFeed = null;
 
 Template.ili_example.events = {
-    'click #subscribe' : function () {
-        var guid = document.getElementById("guid").value;
-        Meteor.call("subscribe", guid, clientId, function(error) {
-            
-            if (sampleSub) sampleSub.stop();
-
-            sampleSub = Meteor.subscribe("samples", guid);
-            
-            if (observer) observer.stop();
-
-            observer = Samples.find().observeChanges({
-                changed: function(id, fields) {
-                    updateSample(fields.value);
-                }
-            });
-        });
+  'click #subscribe' : function () {
+    if (currentFeed) {
+      currentFeed.feed.samples.removeChangedObserver(currentFeed.observer);
     }
+    var guid = document.getElementById("guid").value;
+    var feed = ili_FeedCache.get(guid);
+    feed.load(function(){
+      console.log("Feed loaded: " + feed.tags);
+    });
+
+    var observer = function(inserted, removed) {
+      updateSample(feed.samples.lastValue);
+    }
+
+    currentFeed = { feed: feed, observer: observer };
+
+    feed.samples.onchanged(observer);
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,138 +1,16 @@
 //
-// Sample Intelligent.li application that using Meteor
+// Sample Intelligent.li application using Meteor
 // 
-// Currently this only provides the latest value for the 
-// feed to the client, which means it doesn't support charts etc.
-// this is something we'll improve in time. 
-Feeds = new Meteor.Collection("feeds");
-Samples = new Meteor.Collection("samples");
-Connections = new Meteor.Collection("connections");
-
-//maintains the feed that each client is currently subscribed to.
-var clientObservers = {};
-
-var WebSocket = Meteor.require('ws');
 
 //certificates for accessing ili
 var clientPem = process.env.CERT || Assets.getText('client.pem');
 var keyPem = process.env.KEY || Assets.getText('key.pem');
 var caPem = Assets.getText('intelligent.li-ca.crt');
-
-function publishLatestSample(feed) {
-    Samples.update({ _id: feed.id }, { value: feed.samples.lastValue });
-}
-
-//load the list of feeds from the ili service, 
-//TODO: this needs to be moved into the Intelligent.li.js 
-//helper
-function loadFeeds() {
-
-    ili_feedCache.query("*", Meteor.bindEnvironment(function(result) {
-        Samples.remove({}); 
-        Feeds.remove({});
-        
-        result.each(function(k, v) {
-            Feeds.insert({ _id: v });
-            var feed = ili_feedCache.get(v);
-            feed.load(function(result) { 
-                if (!result) { console.error("failed to load resource"); }
-                else { console.log(feed.tags.items); }
-            });
-        });
-    }, function () { 
-        console.error('Failed to bind environment in loadFeeds()'); 
-    }));
-}
-
-//called remotely by clients when they want to subscribe to the value for a 
-//particular feed.
-function subscribe(guid, clientId) {
-    console.log("subscribing to: " + guid);
-    
-    var feed = ili_feedCache.get(guid);
-
-    if (clientObservers[clientId]) {
-        if (clientObservers[clientId].feed.id == guid) {
-            console.log("this client is already subscribed to this feed");
-            return;
-        } else {
-            removeClientSubscriptions(clientId);
-        }
-    }
-    
-    if (feed.samples.hasChangedObserver()) {
-        console.log("there's already someone watching this feed");
-        Samples.update({ _id: guid }, {$addToSet: { subscribers: clientId }});
-    } else {
-        console.log("we're the first to watch this feed");
-        Samples.remove({ _id: guid });
-        Samples.insert({ _id: guid, value: 0, subscribers : [ clientId ] });
-    }
-    
-    var observer = Meteor.bindEnvironment(function() {
-        console.log("got samples for feed " + feed.id);
-        publishLatestSample(feed);
-    }, function (e) { console.error('Failed to bind environment: ' + e.toString()); });
-
-    clientObservers[clientId] = { feed: feed, onchanged: observer };
-    console.log("feed.attributes.items: " + feed.attributes.items);
-    console.log("feed.attributes.lastsample: " + feed.attributes.get('lastSample'));
-    var ls = feed.attributes.get('lastSample');
-    var interval = feed.attributes.get('interval');
-    if (ls && interval) feed.start = ls - interval;
-    feed.samples.onchanged(observer);
-}
-
-function removeClientSubscriptions(clientId) {
-    console.log("removing subscription for client " + clientId);
-    Samples.update({ subscriptions: clientId }, { $pull: { subscriptions: clientId } } );
-    if (clientObservers[clientId] && clientObservers[clientId].feed) {
-        clientObservers[clientId].feed.samples.removeChangedObserver(clientObservers[clientId].onchanged);
-        clientObservers[clientId] = {}
-    }
-}
-
-function keepalive(clientId) {
-    if (!Connections.findOne(clientId)) {
-        Connections.insert({ _id: clientId});
-    }    
-    console.log("got keepalive from " + clientId);
-    Connections.update(clientId, {$set: {last_seen: (new Date()).getTime()}});
-}
-
-// clean up missing clients after 60 seconds
-Meteor.setInterval(function () {
-    var now = (new Date()).getTime();
-    Connections.find({last_seen: {$lt: (now - 60 * 1000)}}).forEach(function (connection) {
-        console.log("client " + connection._id + " has gone away");
-        removeClientSubscriptions(connection._id);
-        Connections.remove({ _id: connection._id });
-    });
-}, 10000);
-
-function getSamplesForFeed(feedGuid) {
-    return Samples.find({ _id : feedGuid });
-}
+var WebSocket = Meteor.require('ws');
 
 Meteor.startup(function () {
-    
-    Connections.remove({});
-    ili_api.certs = { cert: clientPem, key: keyPem, ca: caPem }; 
-    ili_api.start(function(uri) {
-        return new WebSocket(uri, ili_api.certs);
-    });
-
-    Meteor.methods({
-        subscribe: subscribe, 
-        keepalive: keepalive
-    });
-
-    loadFeeds();
-
-    Meteor.publish("feeds", function() {
-        return Feeds.find();
-    });
-
-    Meteor.publish("samples", getSamplesForFeed);
-
+  ili_Api.certs = { cert: clientPem, key: keyPem, ca: caPem }; 
+  ili_Api.start(function(uri) {
+    return new WebSocket(uri, ili_Api.certs);
+  });
 });
